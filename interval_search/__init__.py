91a83d87c4870006fcbe54c3f1f667b5a96113ce
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-15 15:16:18 -0700

    Bump version: 0.1.2 → 0.2.0

--- interval_search/__init__.py
+++ interval_search/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matthew Andres Moreno"""
 __email__ = 'm.more500@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 from .binary_search import binary_search
 from .doubling_search import doubling_search
