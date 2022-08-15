5567b7b2adb0423c241531a13db5dbf456aec0ca
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-15 15:12:49 -0700

    Implement, test interval_search

--- interval_search/__init__.py
+++ interval_search/__init__.py
@@ -6,9 +6,11 @@ __version__ = '0.1.2'
 
 from .binary_search import binary_search
 from .doubling_search import doubling_search
+from .interval_search import interval_search
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
     'binary_search',
     'doubling_search',
+    'interval_search',
 ]
