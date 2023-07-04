a89397634c7e2af299a715a7c16695e7154b97d1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-03 19:14:57 -0700

    Implement, test satisfaction_bound

--- interval_search/doubling_search.py
+++ interval_search/doubling_search.py
@@ -6,6 +6,7 @@ from .curried_doubling_search import curried_doubling_search
 def doubling_search(
     predicate: typing.Callable[[int], bool],
     lower_bound: int = 0,
+    satisfaction_bound: typing.Optional[int] = None,
 ) -> int:
     """Find the positive integer threshold below which a search criteria is
     never satisfied and above which it is always satisfied.
@@ -17,11 +18,17 @@ def doubling_search(
     lower_bound : int, optional
         The initial guess. Should be less than or equal to the first value that
         satsfies the search criteria. Used for recursion. Default is 0.
+    satisfaction_bound : int, default None
+        Any integer values at or past this value are considered to satisfy the
+        search criteria without calling predicate.
 
     Returns
     -------
     threshold
         The lowest integer value that satisfies the search criteria.
     """
-
-    return curried_doubling_search(predicate)(lower_bound)
+    if satisfaction_bound is not None:
+        predicate_ = lambda x: (x >= satisfaction_bound) or predicate(x)
+    else:
+        predicate_ = predicate
+    return curried_doubling_search(predicate_)(lower_bound)
--- tests/test_doubling_search.py
+++ tests/test_doubling_search.py
@@ -19,3 +19,13 @@ def test_doubling_search_nontrivial():
     assert inch.doubling_search(lambda x: x >= 422, 10) == 422
     assert inch.doubling_search(lambda x: x >= 423) == 423
     assert inch.doubling_search(lambda x: x >= 423, 10) == 423
+
+def test_doubling_search_nontrivial_satisfaction_bound():
+    assert inch.doubling_search(lambda x: x >= 5, satisfaction_bound = 3) == 3
+    assert inch.doubling_search(lambda x: x >= 5, satisfaction_bound = 5) == 5
+    assert inch.doubling_search(lambda x: x >= 5, satisfaction_bound = 6) == 5
+    assert inch.doubling_search(lambda x: x >= 5, 10, 0) == 10
+    assert inch.doubling_search(lambda x: x >= 50, 10, 0) == 10
+    assert inch.doubling_search(lambda x: x >= 422, 10, 10) == 10
+    assert inch.doubling_search(lambda x: x >= 422, 10, 422) == 422
+    assert inch.doubling_search(lambda x: x >= 422, 10, 430) == 422
