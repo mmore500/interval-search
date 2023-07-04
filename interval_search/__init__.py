c1f1d91909829ccfda7116060c04a6dea912cf86
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-03 18:46:50 -0700

    Bump version: 0.3.1 → 0.4.0

--- interval_search/__init__.py
+++ interval_search/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matthew Andres Moreno"""
 __email__ = 'm.more500@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 from .binary_search import binary_search
 from .binary_search_iterative import binary_search_iterative
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/interval-search',
-    version='0.3.1',
+    version='0.4.0',
     zip_safe=False,
 )
